eadcb5513a64fbd575c2b44566e99dffefe89cf8
khalid-serar <khalid.serar@student.moringaschool.com>
2021-12-11 10:36:19 +0300

    modify settings

--- awards/settings.py
+++ awards/settings.py
@@ -14,7 +14,8 @@ import os
 from pathlib import Path
 import cloudinary
 import django_heroku
-
+import dj_database_url
+from decouple import config,Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
